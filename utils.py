88fb6cb2cb9ffbd7d4920fb111a9b8ff06236696
Илья Карташев <ilya_kartashev@list.ru>
2025-09-14 16:56:32 +0300

    При переносе файла на хаб, часть материала не перенислась, пофиксил вручную (Не были добавлены заглавнные буквы в начале текста)

--- utils.py
+++ utils.py
@@ -2,16 +2,16 @@
 import string
 
 def generate_email():
-    """енератор email адресов"""
+    """Генератор email адресов"""
     username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
     domain = random.choice(['gmail.com', 'yandex.ru', 'mail.ru', 'yahoo.com'])
     return f"{username}@{domain}"
 
 def generate_password(length=10):
-    """енератор паролей"""
+    """Генератор паролей"""
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choices(characters, k=length))
 
 def generate_name(length=6):
-    """енератор имен"""
+    """Генератор имен"""
     return ''.join(random.choices(string.ascii_letters, k=length)).capitalize()
